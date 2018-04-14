3846b77e7331aa7bd1470265cee3d9339433a11e
Jake Lee <jakehlee1@gmail.com>
2018-04-14 04:36:46 -0400

    Rocket now sends whenever on

--- Nano_Altimeter_Sender/Nano_Altimeter_Sender.ino
+++ Nano_Altimeter_Sender/Nano_Altimeter_Sender.ino
@@ -17,7 +17,12 @@ void setup() {
   Serial.println("sensor init");
   if (! baro.begin()) {
     Serial.println("no sensor");
-    return;
+    while(1) {
+      digitalWrite(13, HIGH);
+      delay(100);
+      digitalWrite(13, LOW);
+      delay(100);
+    }
   }
 }
 
@@ -25,6 +30,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   float alt = baro.getAltitude();
   Serial.println("alt = " + String(alt,2));
+  rocketSerial.print(String(alt,2));
+  /*
   if(rocketSerial.available() > 1 && !send_flag) {
     String rocket_input = rocketSerial.readString();
     Serial.println("input="+rocket_input);
@@ -40,7 +47,7 @@ void loop() {
     Serial.println("sending: " + String(alt,2));
     rocketSerial.print(String(alt,2));
   }
-  
+  */
   delay(1000);
 }
 
